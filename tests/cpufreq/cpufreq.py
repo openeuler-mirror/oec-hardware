1f45220c542c0e2e231bbe4fc9359d0a7c0b1bfe
Air9 <zhangwenhao8@huawei.com>
2020-06-04 20:36:54 +0800

    update hwcert to hwcompatible

--- tests/cpufreq/cpufreq.py
+++ tests/cpufreq/cpufreq.py
@@ -15,9 +15,9 @@
 from random import randint
 from time import sleep
 
-from hwcert.env import CertEnv
-from hwcert.test import Test
-from hwcert.command import Command, CertCommandError
+from hwcompatible.env import CertEnv
+from hwcompatible.test import Test
+from hwcompatible.command import Command, CertCommandError
 
 
 class CPU:
